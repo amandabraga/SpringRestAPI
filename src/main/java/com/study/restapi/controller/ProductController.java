4bd4004190a8e4ae4bf401ecacf6924a786860e1
Amanda Braga <amandabraga100@gmail.com>
2021-11-22 19:18:33 -0300

    Changes error handling for product not found
    - Removes specific exception for product not found,
    replacing it for Spring's ResponseStatusException

--- src/main/java/com/study/restapi/controller/ProductController.java
+++ src/main/java/com/study/restapi/controller/ProductController.java
@@ -1,12 +1,13 @@
 package com.study.restapi.controller;
 
 import com.study.restapi.controller.dto.ProductDto;
-import com.study.restapi.controller.errors.ProductNotFoundException;
 import com.study.restapi.model.Product;
 import com.study.restapi.repository.ProductRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -27,8 +28,7 @@ public class ProductController {
 
     @GetMapping("/{id}")
     Product getProductById(@PathVariable long id) {
-        return productRepository.findById(id)
-                .orElseThrow(() -> new ProductNotFoundException(id));
+        return findProductById(id);
     }
 
     @PostMapping
@@ -40,19 +40,30 @@ public class ProductController {
                 .body(productSaved);
     }
 
-    @PutMapping
-    Product updateProduct(@RequestBody Product product) {
-        long productId = product.getId();
-        productRepository
-                .findById(productId)
-                .orElseThrow(() -> new ProductNotFoundException(productId));
+    @PutMapping("/{id}")
+    Product updateProduct(@PathVariable long id, @RequestBody ProductDto productDto) {
+        Product product = findProductById(id);
+
+        product.setName(productDto.getName());
+        product.setDescription(productDto.getDescription());
+        product.setBrand(productDto.getBrand());
+        product.setDepartment(productDto.getDepartment());
 
         return productRepository.save(product);
     }
 
     @DeleteMapping("/{id}")
     void deleteProduct(@PathVariable long id) {
+        findProductById(id);
+
         productRepository.deleteById(id);
+    }
+
+    private Product findProductById(long id) {
+        return productRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Product not found"));
     }
 
 }
--- src/main/java/com/study/restapi/controller/errors/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.study.restapi.controller.errors;
-
-public class ProductNotFoundException extends RuntimeException {
-
-    public ProductNotFoundException(Long id) {
-        super("Product " + id + " could not be found");
-    }
-
-}
